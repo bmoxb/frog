3214ebe3e6b38bf05a94998501e8c813b8de967c
bmoxb <maxoblack@yahoo.com>
2023-10-19 21:12:17 +0100

    Use buffers instead of concatenating strings to construct identifiers and literals.

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -12,7 +12,15 @@ let is_identifier = function
   | _ -> false
 
 let is_digit = function '0' .. '9' -> true | _ -> false
+
 let is_whitespace = function ' ' | '\n' | '\t' -> true | _ -> false
+
+let new_buf () = Buffer.create 10
+
+let char_to_buf c =
+  let buf = new_buf () in
+  Buffer.add_char buf c;
+  buf
 
 let peek lexer =
   if lexer.index < String.length lexer.input then Some lexer.input.[lexer.index]
@@ -40,27 +48,36 @@ let rec skip_whitespace lexer =
   | Some peeked when is_whitespace peeked -> lexer |> advance |> skip_whitespace
   | _ -> lexer
 
-let rec handle_string lexeme lexer =
+let rec handle_string lexeme_buf lexer =
   match peek lexer with
-  | Some '"' -> (advance lexer, Token.StringLiteral lexeme)
+  | Some '"' ->
+      let lexeme = Buffer.contents lexeme_buf in
+      (advance lexer, Token.StringLiteral lexeme)
   | Some peeked ->
-      lexer |> advance |> handle_string (lexeme ^ String.make 1 peeked)
+      Buffer.add_char lexeme_buf peeked;
+      lexer |> advance |> handle_string lexeme_buf
   | None -> (lexer, Token.Invalid Token.UnexpectedEOF)
 
-let rec handle_number lexeme ?(is_decimal = false) lexer =
+let rec handle_number ?(is_decimal = false) lexeme_buf lexer =
   match peek lexer with
   | Some peeked when is_digit peeked ->
-      lexer |> advance
-      |> handle_number (lexeme ^ String.make 1 peeked) ~is_decimal
+      Buffer.add_char lexeme_buf peeked;
+      lexer |> advance |> handle_number ~is_decimal lexeme_buf
   | Some '.' when not is_decimal ->
-      lexer |> advance |> handle_number (lexeme ^ ".") ~is_decimal:true
-  | _ -> (lexer, Token.NumberLiteral lexeme)
+      Buffer.add_char lexeme_buf '.';
+      lexer |> advance |> handle_number ~is_decimal:true lexeme_buf
+  | _ ->
+      let lexeme = Buffer.contents lexeme_buf in
+      (lexer, Token.NumberLiteral lexeme)
 
-let rec handle_identifier lexeme lexer =
+let rec handle_identifier lexeme_buf lexer =
   match peek lexer with
   | Some peeked when is_identifier peeked ->
-      lexer |> advance |> handle_identifier (lexeme ^ String.make 1 peeked)
-  | _ -> (lexer, Token.lookup_identifier_or_keyword lexeme)
+      Buffer.add_char lexeme_buf peeked;
+      lexer |> advance |> handle_identifier lexeme_buf
+  | _ ->
+      let lexeme = Buffer.contents lexeme_buf in
+      (lexer, Token.lookup_identifier_or_keyword lexeme)
 
 let make_token kind lexer : Token.t =
   {
@@ -109,9 +126,9 @@ let next_token lexer =
             conditionally_advance
               (fun c -> c = '=')
               ~if_match:LessThanOrEqual ~otherwise:LessThan lexer
-        | '"' -> handle_string "" lexer
-        | c when is_digit c -> handle_number (String.make 1 c) lexer
-        | c when is_identifier c -> handle_identifier (String.make 1 c) lexer
+        | '"' -> handle_string (new_buf ()) lexer
+        | c when is_digit c -> handle_number (char_to_buf c) lexer
+        | c when is_identifier c -> handle_identifier (char_to_buf c) lexer
         | _ -> (lexer, Invalid (UnexpectedChar c))
       in
       (lexer, Some (make_token token_kind lexer))
