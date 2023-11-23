e0bb30f2818a3eceaa8c18874c83d7f42828cc58
bmoxb <maxoblack@yahoo.com>
2023-11-23 17:31:00 +0000

    Remove Ast.mli.

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,12 +1,19 @@
-type expr = [%import: Ast.expr]
+type expr =
+  | BinaryExpr of Token.t * expr * expr
+  | UnaryExpr of Token.t * expr
+  | Group of expr
+  | Literal of Token.t
+[@@deriving show]
 
-let rec make_indent level dot =
-  if level > 0 then
-    let tab = if dot then ". " else ", " in
-    tab ^ make_indent (level - 1) (not dot)
-  else ""
-
+(* Construct a human-readable representation of an expression that shows just
+   token kinds in a simple tree structure. *)
 let show_expr_simple expr =
+  let rec make_indent level dot =
+    if level > 0 then
+      let tab = if dot then ". " else ", " in
+      tab ^ make_indent (level - 1) (not dot)
+    else ""
+  in
   let rec make_representation expr indent_level =
     let indent = make_indent indent_level true in
     match expr with
--- lib/ast.mli
+++
@@ -1,9 +0,0 @@
-type expr =
-  | BinaryExpr of Token.t * expr * expr
-  | UnaryExpr of Token.t * expr
-  | Group of expr
-  | Literal of Token.t
-
-(* Construct a human-readable representation of an expression that shows just
-   token kinds in a simple tree structure. *)
-val show_expr_simple : expr -> string
