8c432457e28714d783dbffeced4e095fb5a64b23
bmoxb <maxoblack@yahoo.com>
2023-11-24 19:53:31 +0000

    Add let expressions and adjust AST representation.

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,8 +1,25 @@
 type expr =
-  | BinaryExpr of Token.t * expr * expr
-  | UnaryExpr of Token.t * expr
-  | Group of expr
+  | LetBinding of {
+      let_token : Token.t;
+      identifier : Token.t;
+      equals_token : Token.t;
+      bound_expr : expr;
+      in_token : Token.t;
+      body_expr : expr;
+    }
+  | BinaryExpr of {
+      operator_token : Token.t;
+      left_expr : expr;
+      right_expr : expr;
+    }
+  | UnaryExpr of { operator_token : Token.t; expr : expr }
+  | Group of {
+      open_bracket_token : Token.t;
+      close_bracket_token : Token.t;
+      expr : expr;
+    }
   | Literal of Token.t
+  | Identifier of Token.t
 [@@deriving show]
 
 (* Construct a human-readable representation of an expression that shows just
@@ -17,16 +34,23 @@ let show_expr_simple expr =
   let rec make_representation expr indent_level =
     let indent = make_indent indent_level true in
     match expr with
-    | BinaryExpr (tok, left, right) ->
-        let show_left = make_representation left (indent_level + 1) in
-        let show_right = make_representation right (indent_level + 1) in
-        indent ^ Token.show_kind tok.kind ^ "\n" ^ show_left ^ "\n" ^ show_right
-    | UnaryExpr (tok, right) ->
-        let show_right = make_representation right (indent_level + 1) in
-        indent ^ Token.show_kind tok.kind ^ "\n" ^ show_right
-    | Group grouped ->
-        let show_grouped = make_representation grouped (indent_level + 1) in
-        indent ^ "group\n" ^ show_grouped
-    | Literal tok -> indent ^ Token.show_kind tok.kind
+    | LetBinding { identifier; bound_expr; body_expr; _ } ->
+        let show_ident = Token.show_kind identifier.kind in
+        let show_bound = make_representation bound_expr (indent_level + 1) in
+        let show_body = make_representation body_expr (indent_level + 1) in
+        indent ^ "let " ^ show_ident ^ " =\n" ^ show_bound ^ "\n" ^ indent
+        ^ "in\n" ^ show_body
+    | BinaryExpr { operator_token; left_expr; right_expr } ->
+        let show_token = Token.show_kind operator_token.kind in
+        let show_left = make_representation left_expr (indent_level + 1) in
+        let show_right = make_representation right_expr (indent_level + 1) in
+        indent ^ show_token ^ "\n" ^ show_left ^ "\n" ^ show_right
+    | UnaryExpr { operator_token; expr } ->
+        let show_expr = make_representation expr (indent_level + 1) in
+        indent ^ Token.show_kind operator_token.kind ^ "\n" ^ show_expr
+    | Group { expr; _ } ->
+        let show_expr = make_representation expr (indent_level + 1) in
+        indent ^ "group\n" ^ show_expr
+    | Literal tok | Identifier tok -> indent ^ Token.show_kind tok.kind
   in
   make_representation expr 0
--- lib/parser.ml
+++ lib/parser.ml
@@ -18,66 +18,93 @@ let advance parser =
    Otherwise, produce an error. *)
 let expect_kind kind parser =
   let parser, token = advance parser in
-  if token.kind = kind then parser else exit 0 (* TODO *)
+  if token.kind = kind then (parser, token) else exit 0 (* TODO *)
 
 let rec parse_expr parser =
-  let parser, expr = parse_logical parser in
-  (parser, Some expr)
+  if List.is_empty parser.tokens then (parser, None)
+  else
+    let parser, expr = expr parser in
+    (parser, Some expr)
 
-and parse_logical parser =
+and expr parser = let_binding parser
+
+and let_binding parser =
+  let open Token in
+  match peek_kind parser with
+  | Some LetKeyword -> (
+      let parser, let_token = advance parser in
+      match advance parser with
+      | parser, ({ kind = Identifier _; _ } as identifier) ->
+          let parser, equals_token = expect_kind Equals parser in
+          let parser, bound_expr = expr parser in
+          let parser, in_token = expect_kind InKeyword parser in
+          let parser, body_expr = expr parser in
+          ( parser,
+            Ast.LetBinding
+              {
+                let_token;
+                identifier;
+                equals_token;
+                bound_expr;
+                in_token;
+                body_expr;
+              } )
+      | _ -> exit 0)
+  | _ -> logical parser
+
+and logical parser =
   let open Token in
   let is_op = function AndKeyword | OrKeyword -> true | _ -> false in
-  parse_left_associative_binary_expr parser parse_equality is_op
+  left_associative_binary_expr parser equality is_op
 
-and parse_equality parser =
+and equality parser =
   let is_op = function Token.Equiv | Token.NotEquiv -> true | _ -> false in
-  parse_left_associative_binary_expr parser parse_comparison is_op
+  left_associative_binary_expr parser comparison is_op
 
-and parse_comparison parser =
+and comparison parser =
   let open Token in
   let is_op = function
     | GreaterThan | LessThan | GreaterThanOrEqual | LessThanOrEqual -> true
     | _ -> false
   in
-  parse_left_associative_binary_expr parser parse_term is_op
+  left_associative_binary_expr parser term is_op
 
-and parse_term parser =
+and term parser =
   let is_op = function Token.Minus | Token.Plus -> true | _ -> false in
-  parse_left_associative_binary_expr parser parse_factor is_op
+  left_associative_binary_expr parser factor is_op
 
-and parse_factor parser =
+and factor parser =
   let is_op = function Token.Star | Token.Slash -> true | _ -> false in
-  parse_left_associative_binary_expr parser parse_unary is_op
+  left_associative_binary_expr parser unary is_op
 
-and parse_unary parser =
+and unary parser =
   match peek_kind parser with
   | Some Token.NotKeyword | Some Token.Minus ->
       let parser, token = advance parser in
-      let parser, right = parse_unary parser in
+      let parser, right = unary parser in
       (parser, Ast.UnaryExpr (token, right))
-  | _ -> parse_primary parser
+  | _ -> primary parser
 
-and parse_primary parser =
+and primary parser =
   let open Token in
   match peek_kind parser with
-  | Some (NumberLiteral _) | Some (StringLiteral _) ->
+  | Some (NumberLiteral _) | Some (StringLiteral _) | Some (Identifier _) ->
       let parser, token = advance parser in
       (parser, Ast.Literal token)
-  | Some OpenBracket -> (
-      let parser, _ = advance parser in
-      let parser, expr = parse_expr parser in
-      let parser = expect_kind CloseBracket parser in
-      match expr with Some expr -> (parser, Ast.Group expr) | None -> exit 0
-      (* need an expr inside the brackets, TODO *))
+  | Some OpenBracket ->
+      let parser, open_bracket_token = advance parser in
+      let parser, expr = expr parser in
+      let parser, close_bracket_token = expect_kind CloseBracket parser in
+      (parser, Ast.Group (open_bracket_token, expr, close_bracket_token))
   | _ -> exit 0
 
-and parse_left_associative_binary_expr parser child_expr is_op =
+and left_associative_binary_expr parser child_expr is_op =
   let parser, left = child_expr parser in
   match peek_kind parser with
   | Some kind when is_op kind ->
       let parser, token = advance parser in
       let parser, right =
-        parse_left_associative_binary_expr parser child_expr is_op
+        left_associative_binary_expr parser child_expr is_op
       in
       (parser, Ast.BinaryExpr (token, left, right))
   | _ -> (parser, left)
