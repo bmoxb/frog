5be5011df9390c850a88a77b8ac6eefdaa3b5052
bmoxb <maxoblack@yahoo.com>
2024-02-04 17:52:24 +0000

    Add basic CLI.

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,24 @@
-open Osaka
+let input_path = ref ""
 
-let rec consume_tokens lexer line =
-  match Lexer.next_token lexer with
-  | lexer, Some (Ok tok) -> tok :: consume_tokens lexer line
-  | _, Some (Error err) ->
-      print_endline (Err.display "repl" line err);
-      []
-  | _ -> []
+let debug_tokens_path = ref ""
 
-let rec repl () =
-  let line = read_line () in
-  print_endline "Tokens:";
-  let lexer = Lexer.init line in
-  let tokens = consume_tokens lexer line in
-  List.iter (fun token -> print_endline (Token.show token)) tokens;
-  print_endline "Expression:";
-  let parser = Parser.init line tokens in
-  let msg =
-    match Parser.parse_expr parser with
-    | _, Some (Ok expr) -> Ast.Expr.show expr
-    | _, Some (Error err) -> Err.display "repl" line err
-    | _, None -> "invalid expr"
-  in
-  print_endline (msg ^ "\n");
-  repl ()
+let debug_ast_path = ref ""
 
-let () = repl ()
+let speclist =
+  [
+    ( "--debug-tokens",
+      Arg.Set_string debug_tokens_path,
+      "Write token debug information to the given file." );
+    ( "--debug-ast",
+      Arg.Set_string debug_ast_path,
+      "Write the AST in Graphviz DOT format to the given file." );
+  ]
+
+let anon_fun path = input_path := path
+
+let usage_msg = "osaka [--debug-tokens <file>] [--debug-ast <file>] <input>"
+
+let () =
+  Arg.parse speclist anon_fun usage_msg;
+  if !input_path = "" then print_endline "Please specify an input file."
+  else Run.run !input_path !debug_tokens_path !debug_ast_path
---
+++ bin/run.ml
@@ -0,0 +1,56 @@
+open Osaka
+
+let get_tokens source_code =
+  let rec consume lexer =
+    match Lexer.next_token lexer with
+    | lexer, Some result ->
+        Result.bind result (fun token ->
+            consume lexer |> Result.map (fun tokens -> token :: tokens))
+    | _, None -> Ok []
+  in
+  consume (Lexer.init source_code)
+
+let debug_tokens path tokens =
+  if path <> "" then
+    let text = tokens |> List.map Token.show |> String.concat "\n" in
+    Out_channel.with_open_text path (fun chan ->
+        Out_channel.output_string chan text)
+  else ()
+
+let get_exprs source_code tokens =
+  let rec consume parser =
+    match Parser.parse_expr parser with
+    | parser, Some result ->
+        Result.bind result (fun expr ->
+            consume parser |> Result.map (fun exprs -> expr :: exprs))
+    | _, None -> Ok []
+  in
+  consume (Parser.init source_code tokens)
+
+let debug_ast path expr =
+  if path <> "" then
+    let text = expr |> Ast.Expr.to_graph |> Graph.to_dot in
+    Out_channel.with_open_text path (fun chan ->
+        Out_channel.output_string chan text)
+  else ()
+
+let run input_path debug_tokens_path debug_ast_path =
+  let source_code =
+    try In_channel.with_open_text input_path In_channel.input_all
+    with _ ->
+      Printf.printf "Failed to read input file: %s\n" input_path;
+      exit 0
+  in
+  let tokens_result = get_tokens source_code in
+  try
+    let exprs_result =
+      Result.bind tokens_result (fun tokens ->
+          debug_tokens debug_tokens_path tokens;
+          get_exprs source_code tokens)
+    in
+    match exprs_result with
+    | Ok exprs ->
+        debug_ast debug_ast_path (List.hd exprs);
+        exprs |> List.iter (fun expr -> Ast.Expr.show expr |> print_endline)
+    | Error err -> Err.display input_path source_code err |> print_endline
+  with _ -> print_endline "Failed to write debug output files."
