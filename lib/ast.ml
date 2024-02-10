a3e691082a6605668014f0d6330e4142af52451b
bmoxb <maxoblack@yahoo.com>
2024-02-10 16:58:06 +0000

    Generate Graphviz trees for top-level definitions and fix CLI.

--- lib/ast.ml
+++ lib/ast.ml
@@ -118,9 +118,8 @@ module Expr = struct
   and kind =
     (* "let" pattern ":" type "=" expr "in" expr *)
     | LetIn of Pattern.t * DataType.t * t * t
-    (* "match" expr "with" [ "|" ] match_arm { "|" match_arm }
-       match_arm = pattern "->" expr *)
-    | Match of t * (Pattern.t * t) list
+    (* "match" expr "with" [ "|" ] match_arm { "|" match_arm } *)
+    | Match of t * match_arm list
     (* "if" expr "then" expr "else" expr *)
     | IfThenElse of t * t * t
     (* expr binary_operator expr *)
@@ -132,11 +131,14 @@ module Expr = struct
     | Primary of primary_kind * string
   [@@deriving show]
 
+  (* pattern "->" expr *)
+  and match_arm = Pattern.t * t [@@deriving show]
+
   let rec to_tree_vertex expr =
     let label, edges =
       match expr.kind with
       | LetIn (pattern, data_type, bound_expr, body_expr) ->
-          ( "let",
+          ( "let-in",
             [
               Tree.edge ~label:"pattern" (Pattern.to_tree_vertex pattern);
               Tree.edge ~label:"type" (DataType.to_tree_vertex data_type);
@@ -144,21 +146,11 @@ module Expr = struct
               Tree.edge ~label:"in" (to_tree_vertex body_expr);
             ] )
       | Match (expr, arms) ->
-          let arm_to_edge index (pattern, expr) =
-            Tree.edge
-              (Tree.vertex
-                 ~edges:
-                   [
-                     Tree.edge ~label:"pattern" (Pattern.to_tree_vertex pattern);
-                     Tree.edge ~label:"expr" (to_tree_vertex expr);
-                   ]
-                 (Printf.sprintf "arm %d" index))
-          in
           ( "match",
             Tree.edge ~label:"expr" (to_tree_vertex expr)
-            :: List.mapi arm_to_edge arms )
+            :: List.mapi match_arm_to_edge arms )
       | IfThenElse (condition, then_expr, else_expr) ->
-          ( "if",
+          ( "if-then-else",
             [
               Tree.edge ~label:"condition" (to_tree_vertex condition);
               Tree.edge ~label:"then" (to_tree_vertex then_expr);
@@ -196,6 +188,16 @@ module Expr = struct
     in
     Tree.vertex ~colour:(Tree.rgb 0.1 0.1 1.0) ~edges label
 
+  and match_arm_to_edge index (pattern, expr) =
+    Tree.edge
+      (Tree.vertex
+         ~edges:
+           [
+             Tree.edge ~label:"pattern" (Pattern.to_tree_vertex pattern);
+             Tree.edge ~label:"expr" (to_tree_vertex expr);
+           ]
+         (Printf.sprintf "arm %d" index))
+
   let to_tree expr =
     Tree.init ~horizontal_spacing:1.0 ~vertical_spacing:1.0
       (to_tree_vertex expr)
@@ -203,6 +205,24 @@ end
 
 (* IDENTFIER [ type ] *)
 type data_arm = identifier * DataType.t option [@@deriving show]
+
+let data_arm_to_edge index (constructor_identifier, data_type_opt) =
+  let constructor_edge =
+    Tree.edge ~label:"constructor identifier"
+      (Tree.vertex constructor_identifier)
+  in
+  (* If the data type is some, then this is a single edge in a list.
+     Otherwise, this is an empty list. *)
+  let data_type_edge_list =
+    data_type_opt
+    |> Option.map (fun data_type ->
+           [ data_type |> DataType.to_tree_vertex |> Tree.edge ])
+    |> Option.value ~default:[]
+  in
+  Tree.edge
+    (Tree.vertex
+       ~edges:(constructor_edge :: data_type_edge_list)
+       (Printf.sprintf "arm %d" index))
 
 type kind =
   (* "let" pattern ":" type "=" expr *)
@@ -214,6 +234,31 @@ type kind =
 [@@deriving show]
 
 type t = { position : Position.t; kind : kind } [@@deriving show]
-(** A top-level definition. A full program is comprised of one or more
-    top-level definitions meaning a program's full AST is represented by a list
-    of t. *)
+(** A top-level definition. A program is comprised of one or more top-level
+    definitions meaning a program's full AST is represented by a list of t. *)
+
+let to_tree_vertex node =
+  let label, edges =
+    match node.kind with
+    | Let (pattern, data_type, expr) ->
+        ( "let",
+          [
+            Tree.edge ~label:"pattern" (Pattern.to_tree_vertex pattern);
+            Tree.edge ~label:"type" (DataType.to_tree_vertex data_type);
+            Tree.edge ~label:"expr" (Expr.to_tree_vertex expr);
+          ] )
+    | Alias (identifier, data_type) ->
+        ( "alias",
+          [
+            Tree.edge ~label:"identifier" (Tree.vertex identifier);
+            Tree.edge ~label:"data type" (DataType.to_tree_vertex data_type);
+          ] )
+    | Data (identifier, arms) ->
+        ( "data",
+          Tree.edge ~label:"identifier" (Tree.vertex identifier)
+          :: List.mapi data_arm_to_edge arms )
+  in
+  Tree.vertex ~colour:(Tree.rgb 0.2 0.2 0.2) ~edges label
+
+let to_tree node =
+  Tree.init ~horizontal_spacing:1.0 ~vertical_spacing:1.0 (to_tree_vertex node)
