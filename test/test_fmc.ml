e3f88ce16013c42105a19c955e7add92bb111a41
bmoxb <maxoblack@yahoo.com>
2024-02-22 12:58:36 +0000

    Fix FMC tests.

--- test/test_fmc.ml
+++ test/test_fmc.ml
@@ -1,45 +1,25 @@
-open Osaka
+open Osaka.Fmc
 open OUnit2
 
-(* These examples are taken from 'Example 3.2' in the FMC paper. *)
+let example_1_displayed = "<x>.x"
 
-let example_1_displayed = "<x>.[x].[x]"
+let example_1 = Pop (Lambda, "x", Variable "x")
 
-let example_1 =
-  Fmc.Abstraction
-    {
-      location = None;
-      variable = "x";
-      term =
-        Fmc.Application
-          {
-            arg = Fmc.Variable "x";
-            location = None;
-            func =
-              Fmc.Application
-                {
-                  arg = Fmc.Variable "x";
-                  location = None;
-                  func = Fmc.Jump None;
-                };
-          };
-    }
-
-let example_2_displayed = "<x>.<y>"
+let example_2_displayed = "[10].[5].[2].+; -"
 
 let example_2 =
-  Fmc.Abstraction
-    {
-      location = None;
-      variable = "x";
-      term =
-        Fmc.Abstraction
-          { location = None; variable = "y"; term = Fmc.Jump None };
-    }
+  Choice
+    ( Push
+        ( Variable "10",
+          Lambda,
+          Push (Variable "5", Lambda, Push (Variable "2", Lambda, Variable "+"))
+        ),
+      Star,
+      Variable "-" )
 
 let test_display name expected_string input_term =
   name >:: fun _ ->
-  assert_equal expected_string (Fmc.display input_term) ~printer:(fun s -> s)
+  assert_equal expected_string (display input_term) ~printer:(fun s -> s)
 
 let tests =
   "fmc"
