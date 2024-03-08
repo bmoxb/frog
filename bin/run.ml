33039f17c2e4921883af0ed5fa398ec81007680f
bmoxb <maxoblack@yahoo.com>
2024-03-08 15:55:08 +0000

    Output FMC terms.

--- bin/run.ml
+++ bin/run.ml
@@ -7,12 +7,12 @@ let debug_tokens path tokens =
         Out_channel.output_string chan text)
   else ()
 
-let debug_ast path ast =
+let debug_ast path nodes =
   let top_level_to_edge top_level = Tree.edge (Ast.to_tree_vertex top_level) in
   if path <> "" then
     let tree =
       Tree.vertex
-        ~edges:(List.map top_level_to_edge ast)
+        ~edges:(List.map top_level_to_edge nodes)
         "top-level definitions"
       |> Tree.init
     in
@@ -30,9 +30,9 @@ let run input_path debug_tokens_path debug_ast_path =
   try
     let tokens = Consume.get_tokens source_code in
     debug_tokens debug_tokens_path tokens;
-    let ast = Consume.get_ast source_code tokens in
-    debug_ast debug_ast_path ast;
-    ast |> List.iter (fun node -> Ast.show node |> print_endline)
+    let nodes = Consume.get_ast source_code tokens in
+    debug_ast debug_ast_path nodes;
+    Translate.translate nodes |> Fmc.display |> print_endline
   with
   | Err.Exception err ->
       print_endline (Err.display ~filename:input_path ~source_code err)
